7a3794a19a0b0d773403e04bf2c218579771a1e8
Zaid Hadya <zaid.omar962@gmail.com>
2025-04-12 19:12:50 +0400

    Installing required dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { Client } = require('pg');
+const port = 3000;
+const app = express();
+require('dotenv').config();
+
+
+const client = new Client({
+    
+})
